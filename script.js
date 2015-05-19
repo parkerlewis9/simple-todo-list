0e36019dde2c3a80790cf6ca584184a2db07feae
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-05-18 17:03:35 -0700

    finish homework

---
+++ script.js
@@ -0,0 +1,29 @@
+//Todo App
+
+function addLi() {
+// Select the place on the form where the input is put
+	var input = document.getElementById("textbox")
+// and extract it's value after there's a click
+	var content = input.value;
+// create new li and put the message into it, then attatch it to the ul
+	var newLi = document.createElement("LI");
+	newLi.innerHTML = content;
+	var list = document.querySelector("ul");
+	list.appendChild(newLi)
+}
+
+var button = document.querySelector("button");
+
+button.addEventListener("click", function(event) {
+	addLi();
+});
+
+var textField = document.getElementById("textbox")
+textField.addEventListener("keydown", function(event) {
+	if(event.keyCode === 13) {
+		event.preventDefault();
+		addLi();
+	}
+
+
+});
